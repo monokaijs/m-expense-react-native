57c31c119a4a267d869ad02a11c7e0544361105e
MonokaiJs <monokaijs@gmail.com>
2022-11-14 02:18:41 +0700

    feat auth: add initial base for auth

--- src/screens/Home/HomeHeader.tsx
+++ src/screens/Home/HomeHeader.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import {StyleSheet, TextInput, TouchableOpacity, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import StyledText from '@components/common/Text';
 import {convertFont, getSize} from '@utils/ui.utils';
@@ -9,21 +15,84 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useAppDispatch} from '@redux/store';
 import {loadAppTrips} from '@redux/actions/app.actions';
 import {useNavigation} from '@react-navigation/native';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 export const HomeHeader = () => {
   const navigation = useNavigation<any>();
   const dispatch = useAppDispatch();
   const {colors} = useTheme();
   const styles = useStyles(colors);
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId: '<FROM DEVELOPER CONSOLE>', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      hostedDomain: '', // specifies a hosted domain restriction
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      accountName: '', // [Android] specifies an account name on the device that should be used
+      iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+      googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+      openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+      profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+    });
+  }, []);
+
+  const onSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
   return (
     <View style={styles.outer}>
-      <TouchableOpacity
-        style={styles.actionBtn}
-        onPress={() => {
-          navigation.openDrawer();
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}>
-        <Icon name={'menu'} color={colors.background} size={getSize.m(28)} />
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.actionBtn}
+          onPress={() => {
+            navigation.openDrawer();
+          }}>
+          <Icon name={'menu'} color={colors.background} size={getSize.m(28)} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onSignIn}
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <StyledText>Sign In</StyledText>
+          <Image
+            source={require('@assets/img/default-avatar.png')}
+            style={{
+              marginLeft: getSize.m(8),
+              width: getSize.m(32),
+              height: getSize.m(32),
+              resizeMode: 'contain',
+              borderRadius: getSize.m(32),
+            }}
+          />
+        </TouchableOpacity>
+      </View>
 
       <StatusBarAware />
       <StyledText style={styles.title}>Welcome back,</StyledText>
